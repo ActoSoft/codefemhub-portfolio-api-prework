c83a97ecb8ed08da01f505bd4ab9ecc88cc8770c
Martin Melo <martin.melo.dev.97@gmail.com>
2024-10-19 01:11:49 -0600

    Getting port from .env file, moving dotenv.config() statement outside setupServer to configure before anything

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const cors = require('cors');
 const gzip = require('compression');
 const dotenv = require('dotenv');
 
+dotenv.config();
+
 const app = express();
 
 const setupServer = () => {
-  dotenv.config();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(cors());
@@ -18,7 +19,7 @@ const setupRouter = () => {
   console.log('Router setup done');
 }
 
-const PORT = 5500;
+const PORT = process.env.PORT || 1234;
 
 app.get('/healtcheck', (req, res) => {
   res.status(200).json({ message: 'ok' });
