e3977c3c8d3eef738c6e84d08737de75f004002d
Martin Melo <martin.melo.dev.97@gmail.com>
2024-10-19 07:21:10 -0600

    Fixed typo, fixed status codes when no data, added get one and create endpoints

--- src/portfolios/router.js
+++ src/portfolios/router.js
@@ -1,16 +1,50 @@
 const express = require('express');
-const porfolios = require('../../portfolio-mock');
+const portfolios = require('../../portfolio-mock');
 
 const router = express.Router();
 
 // Get all portfolios
 router.get('/', (req, res) => {
   console.log('Starting get all portfolios endpoint');
-  if (porfolios.length === 0) {
+  if (portfolios.length === 0) {
     // No portfolios, return a 404 not found status code
-    return res.status(404).json({ message: 'No portfolios found' });
+    return res.status(204).json({ message: 'No portfolios found' });
   }
-  return res.status(200).json(porfolios);
+  return res.status(200).json(portfolios);
+});
+
+// Get one portfolio
+// :id indicates a path parameter, you can use it as req.params
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(`Starting get on portfolio endpoint for id: ${id}`);
+  const portfolio = portfolios.find((portfolio) => portfolio.id === Number(id));
+  if (!portfolio) {
+    return res.status(404).json({ message: `Not found portfolio with id: ${id}`});
+  }
+  return res.status(200).json(portfolio);
+});
+
+// Create new portfolio
+router.post('/', (req, res) => {
+  const data = req.body;
+
+  // TODO: Implement validator package, like Joi
+  if (!data.user || !data.name || !data.url) {
+    return res.status(400).json({ message: 'Invalid data' });
+  }
+
+  data.id = portfolios[portfolios.length - 1].id + 1;
+  data.status = 'draft';
+  data.active = true;
+  data.noVisits = 0;
+
+  data.createdAt = new Date();
+  data.updatedAt = new Date();
+
+  portfolios.push(data);
+
+  return res.status(201).json(data);
 });
 
 module.exports = router;
